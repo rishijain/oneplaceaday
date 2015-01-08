db0e6f3f3601e611186047aaf676fdacbf24e57c
Rishi Jain <rishi@joshsoftware.com>
2015-01-08 13:05:24 +0530

    added posts edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,19 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find params[:id]
+  end
+
+  def update
+    @post = Post.find params[:id]
+    if @post.update(permit_params)
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end
+
   def index
     @posts = Post.all.order('created_at DESC')
   end
