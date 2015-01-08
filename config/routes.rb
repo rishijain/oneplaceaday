94187457871e9dbed585f11928bd23e4f469a4c2
Rishi Jain <rishi@joshsoftware.com>
2015-01-09 00:54:56 +0530

    added home controller and about page with fakest content ever.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   resources :posts
-
+  get 'about' => 'home#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
