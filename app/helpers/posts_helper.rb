a96d251330524beef2d7fa6f3d079bd1fd10841e
Rishi Jain <rishi@joshsoftware.com>
2015-01-13 19:49:18 +0530

    added correct condition for post editable

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
-  def post_is_editable?(action)
-    true if action == 'posts'
+  def post_is_editable?(action, user_id)
+    true if action == 'posts' and current_user.id == user_id
   end
 end
