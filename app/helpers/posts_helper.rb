ba848172bfcbdc9e216313420bff83a8b6bf69a6
Rishi Jain <rishi@joshsoftware.com>
2015-01-15 19:22:52 +0530

    improvised the post editable check

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
   def post_is_editable?(action, user_id)
-    true if action == 'posts' and current_user.id == user_id
+    true if action == 'posts' and current_user and current_user.id == user_id
   end
 end
