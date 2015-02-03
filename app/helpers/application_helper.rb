6c38fa740550fbb03691d0e1f32216d7bdfe035d
Rishi Jain <rishi@joshsoftware.com>
2015-02-03 20:28:14 +0530

    added navigation links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def navigation_active_class(params_page, page_number)
+    return 'active' if params_page == page_number.to_s or (params_page.nil? and page_number == 1)
+  end
+
+  def posts_index_page? params
+    params[:controller] == 'posts' and params[:action] == 'index'
+  end
 end
