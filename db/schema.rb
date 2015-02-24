4538cd7f3ddab85b5e005c84cfbc6c46a19e318d
Rishi Jain <rishi@joshsoftware.com>
2015-02-24 18:39:10 +0530

    added a writeup

--- db/schema.rb
+++ db/schema.rb
@@ -25,31 +25,31 @@ ActiveRecord::Schema.define(version: 20150223195616) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
+    t.string   "title",       limit: 255
     t.text     "description"
-    t.string   "place"
-    t.string   "country"
-    t.string   "visited_on"
+    t.string   "place",       limit: 255
+    t.string   "country",     limit: 255
+    t.string   "visited_on",  limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "photo"
+    t.string   "photo",       limit: 255
     t.integer  "user_id"
     t.float    "latitude"
     t.float    "longitude"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email",                  default: ""
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "username",               limit: 255
+    t.string   "email",                              default: ""
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "profile_photo"
