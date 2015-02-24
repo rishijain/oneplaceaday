01f9c3c3c2b0ed1252118368e0ac0a83d2e5ab5b
Rishi Jain <rishi@joshsoftware.com>
2015-02-24 18:58:47 +0530

    added fonts to precompile list

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Oneplaceaday
   class Application < Rails::Application
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile += %w( .svg .eot .woff .ttf)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
