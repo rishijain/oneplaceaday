237894fce6d85f5673c00a1fbad8415781a34501
Rishi Jain <rishi@joshsoftware.com>
2015-02-24 19:03:05 +0530

    commented for deployment issue

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Oneplaceaday
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-    config.assets.precompile += %w( .svg .eot .woff .ttf)
+    #config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    #config.assets.precompile += %w( .svg .eot .woff .ttf)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
