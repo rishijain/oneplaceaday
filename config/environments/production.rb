d7a4223a2f8a8fb85fb47599c41692387b3af3c3
Rishi Jain <rishi@joshsoftware.com>
2015-05-06 00:41:51 +0530

    added host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
 
   # Mail service
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "www.athappyplace.com" }
   config.action_mailer.smtp_settings = {
     :address          => 'smtp.sendgrid.net',
     :port             => 587,
